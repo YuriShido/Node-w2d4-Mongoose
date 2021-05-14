1c9fdf33e16c422d4eece0729e4ecd1325e7dfa3
YuriShido <sydonce57@gmail.com>
2021-05-13 18:03:02 -0700

    connent to mangoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,32 +1,57 @@
 const Product = require('../models/Products')
-const Cart = require('../models/Cart')
 
 
-exports.getProducts = (req,res,next) => {
-    Product.fetchAll().then((products) => {
-        res.render('shop', {
-            pageTitle: 'Shop Page',
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render('shop', {
+        pageTitle: 'Shop Page',
+        products: products,
+      })
+    })
+    .catch((err) => console.log(err))
+}
+
+exports.getOneProductById = (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((product) => {
+      res.render('product', {
+        pageTitle: product.title,
+        product: product,
+      })
+    })
+    .catch((err) => console.log(err))
+}
+
+exports.getCart = (req, res, next) => {
+    // console.log(req.user);
+    // res.json({msg: 'ok'})
+    req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then((user) => {
+        const products = user.cart.items
+        res.render('shop/cart', {
+            pageTitle: 'Your Cart',
             products: products
         })
     }).catch(err => console.log(err))
-    
 }
 
-exports.getOneProductById = (req,res,next) => {
-    Product.findById(req.params.id).then((product) => {
-        console.log(product)
-        res.render('product', {
-            pageTitle: product.title,
-            product: product
-        })
-    }).catch(err => console.log(err))
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.findById(prodId)
+  .then((product)=> {
+    return req.user.addToCart(product)
+  }).then(()=>  {
+      res.redirect('/')
+  }).catch(err => console.log(err))
+  
 }
 
-exports.postCart = (req,res,next) => {
+exports.postCartDeleteproduct = (req, res, next) => {
     const prodId = req.body.productId
-    const fetchProduct = Product.fetchOneProductById(prodId)
-
-    Cart.addProduct(fetchProduct.id, fetchProduct.price)
-    // res.render('cart',)
-    res.redirect('/')
+    req.user.removeFromCart(prodId).then((product) => {
+        res.redirect('/cart')
+    }).catch(err => console.log(err))
 }
